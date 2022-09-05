7d010a8ef56c528bdf51e209e2f24fb3014db058
Abdulsatar <toyotacamry@windowslive.com>
2022-09-05 19:40:19 +0300

    Day1 Class work

---
+++ Day1.java
@@ -0,0 +1,19 @@
+/* Online Java Compiler and Editor */
+public class HelloWorld{
+
+     public static void main(String []args){
+         //Excercise 1
+        int age= 32;
+        String name= "Abdulsatar";
+        System.out.println("Hi, My name is "+name+" and I am "+age+" years old"+"\n");
+        
+    
+    //Excercise 2
+    double height= 170.5;
+        if(height>160){
+            System.out.println("Your height is above 160, you can enter");
+        }else{
+            System.out.println("Your height is less than160");
+            } 
+}
+}
